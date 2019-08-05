134f079db49c97e4f2d6c928a8007d35077c064f
Vitor de Souza <souza.vitor@outlook.com>
2019-08-05 10:00:19 -0400

    Tidy up some things

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
 const mongoose = require("mongoose");
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const UserService = require('../services/user.service');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const UserService = require("../services/user.service");
 
-const validator = require('express-validator');
+const validator = require("express-validator");
 
-const AuthService = require('../services/auth.service');
-const EmailService = require('../services/email.service');
+const AuthService = require("../services/auth.service");
+const EmailService = require("../services/email.service");
 
 /**
  * Take each handler, and attach to one of the Express.Router's
@@ -22,41 +22,43 @@ function registerRoutes(router) {
   registerMiddleware(router);
 
   // public routes
-  router.post('/authenticate', authenticate);
-  router.post('/', createUser);
-  // router.post('/send-email-verification/:_id', sendEmailVerification);
+  router.post("/authenticate", authenticate);
+  router.post("/", createUser);
+  router.post("/send-email-verification/:_id", sendEmailVerification);
 
   // private routes
-  router.get('/', AuthService.authenticate(), getAll);
-  router.get('/:_id', AuthService.authenticate(), getById);
-  router.put('/:_id', AuthService.authenticate(), updateUser);
-  router.delete('/:_id', AuthService.authenticate(), removeUser);
+  router.get("/", AuthService.authenticate(), getAll);
+  router.get("/:_id", AuthService.authenticate(), getById);
+  router.put("/:_id", AuthService.authenticate(), updateUser);
+  router.delete("/:_id", AuthService.authenticate(), removeUser);
   // router.get('/check-email-verification/:token', AuthService.authenticate(), checkEmailVerification);
 }
 
 function registerMiddleware(router) {
-  router.use(validator({
-    customValidators: {
-      isUsernameTaken : (param, _id) => {
-        return new Promise((resolve, reject) => {
-          let conditions = {username: param};
-          if (typeof _id !== 'undefined') {
-            conditions['_id'] = {$ne: _id};
-          }
-          return UserService.findOne(conditions)
-            .then(user => {
-              if (user) {
-                return reject(user);
-              }
-              return resolve(true);
-            })
-            .catch(err => {
-              resolve(err);
-            });
-        });
+  router.use(
+    validator({
+      customValidators: {
+        isUsernameTaken: (param, _id) => {
+          return new Promise((resolve, reject) => {
+            let conditions = { username: param };
+            if (typeof _id !== "undefined") {
+              conditions["_id"] = { $ne: _id };
+            }
+            return UserService.findOne(conditions)
+              .then(user => {
+                if (user) {
+                  return reject(user);
+                }
+                return resolve(true);
+              })
+              .catch(err => {
+                resolve(err);
+              });
+          });
+        }
       }
-    }
-  }));
+    })
+  );
 }
 
 /**
@@ -68,12 +70,14 @@ function registerMiddleware(router) {
  * @return void
  */
 function getAll(req, res, next) {
-  UserService.find({}, {password: false}).then(users => {
-    res.send({data : users})
-  }).catch(err => {
-    res.status(500);
-    res.send({data: 'service not available'})
-  });
+  UserService.find({}, { password: false })
+    .then(users => {
+      res.send({ data: users });
+    })
+    .catch(err => {
+      res.status(500);
+      res.send({ data: "service not available" });
+    });
 }
 
 /**
@@ -85,18 +89,22 @@ function getAll(req, res, next) {
  * @return void
  */
 function removeUser(req, res, next) {
-  UserService.findById(req.params._id).then(user => {
-    UserService.remove({_id : user._id}).then(result => {
-      res.status(204);
-      res.send();
-    }).catch(ex => {
+  UserService.findById(req.params._id)
+    .then(user => {
+      UserService.remove({ _id: user._id })
+        .then(result => {
+          res.status(204);
+          res.send();
+        })
+        .catch(ex => {
+          res.status(500);
+          res.send({ data: "service not available" });
+        });
+    })
+    .catch(ex => {
       res.status(500);
-      res.send({data: 'service not available'})
+      res.send({ data: "service not available" });
     });
-  }).catch(ex => {
-    res.status(500);
-    res.send({data: 'service not available'})
-  });
 }
 
 /**
@@ -109,34 +117,43 @@ function removeUser(req, res, next) {
  */
 function createUser(req, res, next) {
   // validate the fields in the body
-  req.checkBody('email', 'Email is invalid').isEmail();
-  req.checkBody('username')
-    .notEmpty().withMessage('Username is required')
-    .isUsernameTaken().withMessage('Username already registered');
-  req.checkBody('password', 'Password is required').notEmpty();
-  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody("email", "Email is invalid").isEmail();
+  req
+    .checkBody("username")
+    .notEmpty()
+    .withMessage("Username is required")
+    .isUsernameTaken()
+    .withMessage("Username already registered");
+  req.checkBody("password", "Password is required").notEmpty();
+  req.checkBody("name", "Name is required").notEmpty();
 
-  req.asyncValidationErrors().then(() => {
-    let data = req.body;
-    data.checked_email = false;
-    let user = new UserService(data);
-    user.save().then((result) => {
-      res.status(201);
-      res.send({
-        data: {
-          _id : result._id,
-          name : result.name,
-          email : result.email,
-          username : result.username,
-          created_at: result.created_at
-        }
-      });
-    }).catch(err => {
-      res.status(500).send({data : 'something is wrong'});
+  req
+    .asyncValidationErrors()
+    .then(() => {
+      let data = req.body;
+      data.checked_email = false;
+      let user = new UserService(data);
+      user
+        .save()
+        .then(result => {
+          res.status(201);
+          res.send({
+            data: {
+              _id: result._id,
+              name: result.name,
+              email: result.email,
+              username: result.username,
+              created_at: result.created_at
+            }
+          });
+        })
+        .catch(err => {
+          res.status(500).send({ data: "something is wrong" });
+        });
+    })
+    .catch(errors => {
+      res.status(400).send({ data: errors });
     });
-  }).catch(errors => {
-    res.status(400).send({data: errors});
-  });
 }
 
 /**
@@ -151,37 +168,39 @@ function authenticate(req, res, next) {
   if (req.body.username && req.body.password) {
     let username = req.body.username;
     let password = req.body.password;
-    let conditions = {$or : [{email : username}, {username : username}]};
-    UserService.findOne(conditions).then((user) => {
-      if (user) {
-        user.checkPassword(password, (err, isMatch) => {
-          if (err) {
-            res.sendStatus(401);
-          } else if (isMatch) {
-            let payload = {_id: user._id};
-            let token = jwt.sign(payload, process.env.JWT_SECRET);
-            res.json({
-              data: {
-                _id : user._id,
-                email : user.email,
-                name : user.name,
-                username : user.username,
-                token: token
-              }
-            });
-          } else {
-            res.status(401);
-            res.send({error: 'Username and/or password invalid'});
-          }
-        });
-      } else {
-        res.status(401);
-        res.send({error: 'Username and/or password invalid'});
-      }
-    }).catch(err => {
-      res.status(500);
-      res.send({error: 'Something is not right'})
-    });
+    let conditions = { $or: [{ email: username }, { username: username }] };
+    UserService.findOne(conditions)
+      .then(user => {
+        if (user) {
+          user.checkPassword(password, (err, isMatch) => {
+            if (err) {
+              res.sendStatus(401);
+            } else if (isMatch) {
+              let payload = { _id: user._id };
+              let token = jwt.sign(payload, process.env.JWT_SECRET);
+              res.json({
+                data: {
+                  _id: user._id,
+                  email: user.email,
+                  name: user.name,
+                  username: user.username,
+                  token: token
+                }
+              });
+            } else {
+              res.status(401);
+              res.send({ error: "Username and/or password invalid" });
+            }
+          });
+        } else {
+          res.status(401);
+          res.send({ error: "Username and/or password invalid" });
+        }
+      })
+      .catch(err => {
+        res.status(500);
+        res.send({ error: "Something is not right" });
+      });
   } else {
     res.sendStatus(401);
   }
@@ -197,40 +216,47 @@ function authenticate(req, res, next) {
  */
 function updateUser(req, res, next) {
   let data = req.body;
-  UserService.findById(req.params._id).then((user) => {
+  UserService.findById(req.params._id)
+    .then(user => {
+      // validate the fields in the body
+      req.checkBody("email", "Email is invalid").isEmail();
 
-    // validate the fields in the body
-    req.checkBody('email', 'Email is invalid').isEmail();
-
-    req.asyncValidationErrors().then(() => {
-      user.name = data.name || user.name;
-      if (typeof data.email !== 'undefined') {
-        user.email = data.email;
-        user.checked_email = false;
-      }
-      user.password = data.password || user.password;
-      user.save().then((newUser) => {
-        res.json({
-          data: {
-            _id : newUser._id,
-            email : newUser.email,
-            name : newUser.name,
-            username : newUser.username,
-            created_at: newUser.created_at,
-            updated_at: newUser.updated_at
+      req
+        .asyncValidationErrors()
+        .then(() => {
+          user.name = data.name || user.name;
+          if (typeof data.email !== "undefined") {
+            user.email = data.email;
+            user.checked_email = false;
           }
+          user.password = data.password || user.password;
+          user
+            .save()
+            .then(newUser => {
+              res.json({
+                data: {
+                  _id: newUser._id,
+                  email: newUser.email,
+                  name: newUser.name,
+                  username: newUser.username,
+                  created_at: newUser.created_at,
+                  updated_at: newUser.updated_at
+                }
+              });
+            })
+            .catch(ex => {
+              res.status(500);
+              res.send({ data: "service not available" });
+            });
+        })
+        .catch(errors => {
+          res.status(400).send({ data: errors });
         });
-      }).catch(ex => {
-        res.status(500);
-        res.send({data: 'service not available'})
-      });
-    }).catch(errors => {
-      res.status(400).send({data: errors});
+    })
+    .catch(ex => {
+      res.status(500);
+      res.send({ data: "service not available" });
     });
-  }).catch(ex => {
-    res.status(500);
-    res.send({data: 'service not available'})
-  });
 }
 
 /**
@@ -242,12 +268,14 @@ function updateUser(req, res, next) {
  * @return void
  */
 function getById(req, res, next) {
-  UserService.findById(req.params._id).then(user => {
-    res.send({data: user});
-  }).catch(ex => {
-    res.status(500);
-    res.send({data: 'service not available'})
-  });
+  UserService.findById(req.params._id)
+    .then(user => {
+      res.send({ data: user });
+    })
+    .catch(ex => {
+      res.status(500);
+      res.send({ data: "service not available" });
+    });
 }
 
 /**
@@ -260,29 +288,40 @@ function getById(req, res, next) {
  */
 function sendEmailVerification(req, res, next) {
   let key = bcrypt.genSaltSync(8);
-  UserService.findById(req.params._id).then((user) => {
-    user.email_verification_token = key;
-    user.save().then((savedUser) => {
-      let email = new EmailModel();
-      email.to = user.email;
-      email.template = "email-verification";
-      email.subject = "Hey, please check your email!";
-      email.data = savedUser;
-      EmailService.queueEmail(email).then((result) => {
-        res.status(201);
-        res.send({data: 'OK'});
-      }).catch(ex => {
-        res.status(500);
-        res.send({data: 'service not available. Queue error.'})
-      });
-    }).catch(ex => {
+  UserService.findById(req.params._id)
+    .then(user => {
+      user.email_verification_token = key;
+      user
+        .save()
+        .then(savedUser => {
+          let email = new EmailModel();
+          email.to = user.email;
+          email.template = "email-verification";
+          email.subject = "Hey, please check your email!";
+          email.data = savedUser;
+          EmailService.queueEmail(email)
+            .then(result => {
+              res.status(201);
+              res.send({ data: "OK" });
+            })
+            .catch(ex => {
+              res.status(500);
+              res.send({ data: "service not available. Queue error." });
+            });
+        })
+        .catch(ex => {
+          res.status(500);
+          res.send({
+            data: "service not available. Database error save token"
+          });
+        });
+    })
+    .catch(ex => {
       res.status(500);
-      res.send({data: 'service not available. Database error save token'})
+      res.send({
+        data: "service not available. Database error could not find user"
+      });
     });
-  }).catch(ex => {
-    res.status(500);
-    res.send({data: 'service not available. Database error could not find user'})
-  });
 }
 
 /**
@@ -296,31 +335,37 @@ function sendEmailVerification(req, res, next) {
 function checkEmailVerification(req, res, next) {
   if (!req.params.token) {
     res.status(400);
-    res.send({data: [{param: 'token', msg: 'verification token value is missing'}]});
+    res.send({
+      data: [{ param: "token", msg: "verification token value is missing" }]
+    });
   }
 
   let key = decodeURIComponent(req.params.token);
   UserService.update(
-    {_id: req.user._id, email_verification_token: key},
-    {$set : {
-      checked_email: true,
-      email_verification_token : ''
-    }}
-  ).then((result) => {
-    if (result.nModified === 1) {
-      res.status(200);
-      res.send({data: 'OK'});
-    } else {
-      res.status(400);
-      res.send({data: 'verification token is not right'});
+    { _id: req.user._id, email_verification_token: key },
+    {
+      $set: {
+        checked_email: true,
+        email_verification_token: ""
+      }
     }
-  }).catch(ex => {
-    res.status(500);
-    res.send({data: 'service not available.'})
-  });
+  )
+    .then(result => {
+      if (result.nModified === 1) {
+        res.status(200);
+        res.send({ data: "OK" });
+      } else {
+        res.status(400);
+        res.send({ data: "verification token is not right" });
+      }
+    })
+    .catch(ex => {
+      res.status(500);
+      res.send({ data: "service not available." });
+    });
 }
 
-var router = require('express').Router();
+var router = require("express").Router();
 
 registerRoutes(router);
 
